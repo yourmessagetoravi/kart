24a324fbdbeff25e804dbfe89496e58e1b91ffce
Ravi <yourmessagetoravi@gmail.com>
2023-04-28 15:56:12 +0530

    Initial commit

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./categories.component.css']
 })
 export class CategoriesComponent {
+  path = "./assets/images/menu/";
+  categs = [
+    {img:"grossery.webp",tit:"Grossery"},
+    {img:"mobiles.webp", tit:"mobiles"},
+    {img:"home.webp", tit:"home"},
+    {img:"appliences.webp", tit:"aappliences"},
+    {img:"beauty and toys.webp", tit:"beauty and toys"},
+    {img:"fashion.webp", tit:"fashion"},
+    {img:"top offers.webp", tit:"Top offers"},
+    {img:"travel.webp", tit:"Travel"},
+    {img:"electronics.webp", tit:"Electronics"}
+  ]
 
 }
